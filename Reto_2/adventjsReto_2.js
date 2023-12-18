a29905ae03985ef55c4a89253670e24469e7e6c0
AdrianH <adriandario3@gmail.com>
2023-12-18 11:41:23 -0300

    En el taller de Santa, los elfos tienen una lista de regalos que desean fabricar y un conjunto limitado de materiales.
    Los regalos son cadenas de texto y los materiales son caracteres. Tu tarea es escribir una función que, dada una lista de regalos y los materiales disponibles, devuelva una lista de los regalos que se pueden fabricar.
    
    Un regalo se puede fabricar si contamos con todos los materiales necesarios para fabricarlo.

---
+++ Reto_2/adventjsReto_2.js
@@ -0,0 +1,44 @@
+// En el taller de Santa, los elfos tienen una lista de regalos que desean fabricar y un conjunto limitado de materiales.
+
+// Los regalos son cadenas de texto y los materiales son caracteres. Tu tarea es escribir una función que, dada una lista de regalos y los materiales disponibles, devuelva una lista de los regalos que se pueden fabricar.
+
+// Un regalo se puede fabricar si contamos con todos los materiales necesarios para fabricarlo.
+
+// const gifts = ['tren', 'oso', 'pelota']
+// const materials = 'tronesa'
+
+// manufacture(gifts, materials) // ["tren", "oso"]
+// // 'tren' SÍ porque sus letras están en 'tronesa'
+// // 'oso' SÍ porque sus letras están en 'tronesa'
+// // 'pelota' NO porque sus letras NO están en 'tronesa'
+
+// const gifts = ['juego', 'puzzle']
+// const materials = 'jlepuz'
+
+// manufacture(gifts, materials) // ["puzzle"]
+
+// const gifts = ['libro', 'ps5']
+// const materials = 'psli'
+
+// manufacture(gifts, materials) // []
+
+
+function manufacture(gifts, materials) {
+  return gifts.filter(gift => {
+    const giftMaterials = new Set(gift);
+    return [...giftMaterials].every(char => materials.includes(char));
+  });
+}
+
+// Ejemplos de uso
+const gifts1 = ['tren', 'oso', 'pelota'];
+const materials1 = 'tronesa';
+console.log(manufacture(gifts1, materials1)); // ["tren", "oso"]
+
+const gifts2 = ['juego', 'puzzle'];
+const materials2 = 'jlepuz';
+console.log(manufacture(gifts2, materials2)); // ["puzzle"]
+
+const gifts3 = ['libro', 'ps5'];
+const materials3 = 'psli';
+console.log(manufacture(gifts3, materials3)); // []
